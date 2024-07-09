17d0a44d9d54783a835636f3957b10503235dc11
imlogang <37604784+imlogang@users.noreply.github.com>
2024-07-08 20:23:17 -0500

    fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+// Get the recommended configuration
+const recommendedConfig = pluginJs.configs.recommended;
+
+// Create a new configuration object
+const newConfig = {
+  ...recommendedConfig,
+  rules: {
+    ...recommendedConfig.rules,
+    "no-unused-vars": "off",
+  },
+};
 
 export default [
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
+  { languageOptions: { globals: globals.node } },
+  newConfig,
 ];
